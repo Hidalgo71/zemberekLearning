f18e7873ce86fade5190e9fd0a067ee8c900655a
Hidalgo71 <y.ozdemir71@gmail.com>
2020-02-26 17:21:43 +0300

    Initial commit

---
+++ src/zemberek/main.java
@@ -0,0 +1,15 @@
+package zemberek;
+import zemberek.morphology.analysis.WordAnalysis;
+import zemberek.morphology.analysis.tr.TurkishMorphology;
+import java.io.IOException;
+import java.util.List;
+
+public class main
+{
+    public static void main(String[] args) throws IOException
+    {
+        TurkishMorphology morphology = TurkishMorphology.createWithDefaults();
+        List<WordAnalysis> results = morphology.analyze("şimdilerde");
+        results.forEach(s -> System.out.println(s.formatLong()));
+    }
+}
